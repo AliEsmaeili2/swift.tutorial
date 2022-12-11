65478b746b22e5f9c1188a95967a5e442ebd1d34
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2022-12-11 20:26:10 +0330

    Step1-Func1
    array-Set-Function

--- MyPlayground test1 seanallen.playground/Contents.swift
+++ MyPlayground test1 seanallen.playground/Contents.swift
@@ -1,6 +1,7 @@
 import UIKit
-import SwiftUI
+import MapKit
 
+//.............................................................Variable
 
 var greeting = "Hello, playground"
 //string
@@ -13,6 +14,8 @@ var b = 5.3
 
 let c = 3
 //stable
+
+//..............................................................Arry
 
 var ages = [23, 24, 45, 22, 66, 24]
 //arry
@@ -50,15 +53,56 @@ ages.shuffle()
 print(ages)
 // random
 
-//...............................set
+//...............................................................set
 
 var agesSet = Set(ages)
 print(agesSet)
 //set will delete Duplicate numbers
 
-agesSet.contains(24)
+agesSet.contains(29)
 print(agesSet)
 // if 24 exist in arry say true
+
+
+agesSet.contains(88)
+print(agesSet)
+agesSet.insert(88)
+// add to arry
+agesSet.contains(88)
+print(agesSet)
+
+//........................................................dictionary
+
+let devices : [String : String] = [
+     
+    "phone": "iphone 7",
+    "laptop": "MacBookPro 2019",
+    "watch": "SE Nike 2021"
+    ]
+//constant time
+devices["laptop"]
+devices["phone"]
+devices["watch"]
+
+
+print(devices)
+
+
+//........................................................Functions
+
+func test1() {
+    
+print("hello iran")
+
+}
+test1()
+
+var weree = 6
+
+
+
+
+
 
 
 
