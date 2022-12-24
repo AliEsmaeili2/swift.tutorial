10e65ad2ea3224157bb20903a07b346775ba09b2
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2022-12-24 22:49:26 +0330

    UIkit

---
+++ UIkit-Intro/UIkit-Intro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIkit-Intro
+//
+//  Created by Ali esmaeili on 12/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
